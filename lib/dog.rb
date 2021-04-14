be7c46b80a96909643597a64235f7d9e41604e2a
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2021-04-14 19:42:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,7 @@
 class Dog
+  attr_accessor :name, :breed
   def initialize(name, breed="Mutt")
     @name=name
     @breed=breed
-  end
-
-  def name=(name)
-    @name=name
-  end
-
-  def name
-    @name
-  end
-
-  def breed=(breed)
-    @breed=breed
-  end
-
-  def breed
-    @breed
   end
 end
